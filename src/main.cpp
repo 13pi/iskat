7f259e1cfa06ca1d3e0785a63a1d8285b7a1694b
Nikita Bakaev <ya@nbakaev.ru>
2015-07-16 13:03:33 +0300

    "ls" like program. Work recursive & print files with metadata

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,82 @@
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/parsers.hpp>
 #include <boost/program_options/variables_map.hpp>
+#include <boost/filesystem.hpp>
+
+#include <stdio.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <iomanip>
 
 char const * version = "0.0.2";
 
 namespace po = boost::program_options;
+
+//TODO: not used
+struct File {
+    File(const std::string&);
+    std::string filename;
+    struct stat buf;
+    /* more fields  */
+};
+
+    void walk( const boost::filesystem::path & dir_path,         // in this directory,
+               int maxdep ){
+      if ( !(exists( dir_path )&&maxdep) ) return;
+      boost::filesystem::directory_iterator end_itr; // default construction yields past-the-end
+      for ( boost::filesystem::directory_iterator itr( dir_path );
+            itr != end_itr;
+            ++itr )
+      {
+        if ( is_directory(itr->status()) )
+        {
+          //std::cout<<itr->path().leaf();
+          walk( itr->path(), maxdep-1);
+        }else // see below{
+
+        char *date;
+        int ret;
+        struct stat buf;
+
+// absolute path !!!
+// itr->path().string().c_str();
+        if ((ret = stat(itr->path().string().c_str(), &buf))!=0){
+            std::cout << "error" << itr->path() << "\r\n";
+            fprintf(stderr, "stat failure error .%d", ret);
+            // abort();
+            return;
+          }
+        date = asctime(localtime(&buf.st_ctime));
+
+        // JUST FILE NAME !!
+     std::cout << std::setw(10) << std::left <<  itr->path().leaf();
+     std::cout << std::setw(5) << std::left << buf.st_size  ;
+     std::cout << std::setw(10) << std::left << date  ;
+     std::cout << std::setw(10) << std::left << buf.st_mode  ;
+     std::cout << std::setw(5) << std::left << buf.st_uid  ;
+     std::cout << std::setw(5) << std::left << buf.st_gid << "\n" ;
+
+
+       // std::cout<<itr->path().leaf() << "\t";
+         //std::cout << buf.st_size << "\t";
+         ///std::cout <<std::setw (25) << std::left <<  date << "\t";
+         //std::cout << buf.st_mode << "\t";
+        // printf("\n %ld size\n", buf.st_size);
+        //printf("\n %s\n", date);
+        //printf("\n %d mode\n", buf.st_mode);
+        //std::cout << "st_uid" << buf.st_uid << "\t";
+        //std::cout << "st_gid" << buf.st_gid << "\t";
+        //std::cout <<std::endl;
+
+        }
+      }
+      return;
+    }
+
+
 
 int main(int argc, char *argv[]) {
 
@@ -76,6 +148,17 @@ int main(int argc, char *argv[]) {
     }
 
     std::cout << max_depth << follow_syml << std::endl;
+
+    std::cout <<"Name" << std::setw(5) 
+    <<"Size"<< std::setw(30) 
+     <<"Date " << std::setw(20) 
+    <<"Mode "<< std::setw(20) 
+    <<"st_uid " << std::setw(20) 
+    <<"st_gid \n"  ;
+    std::cout << std::setfill(' ') << std::setw(140) << " " << std::endl;
+ 
+    boost::filesystem::path cur_dir(".");
+    walk(cur_dir, 7);
 
     return 0;
 }
