c6fdf7ced4b784c15eebb464c29de12ea8b6a1f7
Vadim Andronov <vadimadr@gmail.com>
2015-07-22 00:59:44 +0300

    walker module enhancements

--- src/Walker.cpp
+++ src/Walker.cpp
@@ -1,19 +1,99 @@
 #include "Walker.h"
 
-bool Walker::get_next(std::string & s) {
-	skip_deep();
-	if (cur == end) return false;
-	s = cur->path().string();
-	++cur;
-	return true;
+#include <unistd.h>
+
+#include <iostream>
+#include <cstring>
+
+using std::string;
+
+Walker::Walker(std::string const & start, int md, bool fl) : follow_syml(fl), maxlevel(md), d(NULL) {
+	current = start;
+	level = 1;
+	if (follow_syml) stat(current.c_str(), &st);
+	else lstat(current.c_str(), &st);
+	if (S_ISDIR(st.st_mode) && (maxlevel > 0 || maxlevel < 0)) {
+		if (!eaccess(current.c_str(), R_OK | X_OK))	 {
+			parent = current;
+			if (parent.at(parent.length() - 1) != '/') parent += "/";
+			d = opendir(current.c_str());
+		} else {
+			std::cerr << PROGRAM_NAME << ": `" 
+			<< current 
+			<< "' Acccess denied" << std::endl;
+		}
+
+	}
 }
 
-Walker::Walker(std::string const & start, int md, bool fl) : follow_syml(fl), max_depth(md) {
-	fs::path s_dir(start);
-	cur = fs::recursive_directory_iterator(s_dir);
+bool Walker::get_next(File & f) {
+
+	while (level >= 0) {
+
+		if (d == NULL) {
+			f = File(current);
+			level = -1;
+			return true;
+		}
+
+		if ((dent = readdir(d))) {
+			char const * cur = dent->d_name;
+			if (!strcmp(cur, ".") || !strcmp(cur, "..")) continue;
+
+			// create file
+			f = File(current);
+
+			if (should_be_visited()) push();
+			else current = parent + cur;
+			return true;
+		} 
+		else pop();
+	}
+
+	return false;
 }
 
-void Walker::skip_deep() {
-	if (max_depth == -1) return;
-	while(cur != end && cur.level() > max_depth) ++cur;
+void Walker::pop() {
+	closedir(d);
+	if (--level) {	
+		dent = dent_st.top();
+		d = dir_st.top();
+		parent = parent_st.top();
+
+		dent_st.pop();
+		dir_st.pop();
+		parent_st.pop();
+	} 
+	else d = NULL;
+
+}
+
+void Walker::push() {
+	level += 1;
+	parent_st.push(parent);
+	dir_st.push(d);
+	dent_st.push(dent);
+
+	parent += dent->d_name;
+	current = parent;
+	if (parent.at(parent.length() - 1) != '/') parent += "/";
+	d = opendir(parent.c_str());
+}
+
+bool Walker::should_be_visited() {
+	string full_path;
+
+	if (maxlevel > 0 && level == maxlevel) return false;
+	full_path = parent + dent->d_name;
+	if (follow_syml) stat(full_path.c_str(), &st);
+	else lstat(full_path.c_str(), &st);
+	if (S_ISDIR(st.st_mode)) {
+		if (!eaccess(full_path.c_str(), R_OK | X_OK )) return true; 
+		else {
+			std::cerr << PROGRAM_NAME << ": `" 
+			<< full_path 
+			<< "' Acccess denied" << std::endl;
+		}
+	}
+	return false;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 #include <boost/program_options/options_description.hpp>
@@ -9,9 +10,13 @@
 #include "Filter.h"
 #include "Walker.h"
 
+#include "iskat.h"
+
+char const * PROGRAM_NAME = "iskat";
+
 namespace po = boost::program_options;
 
-char const * VERSION = "0.1.0";
+char const * VERSION = "0.2.0";
 
 void walk(std::string const & start_dir, po::variables_map & po) {
     int max_depth = -1;
@@ -26,23 +31,22 @@ void walk(std::string const & start_dir, po::variables_map & po) {
 
     Walker w(start_dir, max_depth, follow_syml);
     FilterList filters = make_filters(po);
-    std::string current;
+
+    File current(start_dir);
 
     while(w.get_next(current)){
 
         bool pass_filters = true;
 
-        File f(current);
-
         filter_it cur = filters.begin();
         filter_it end = filters.end();
 
         for (;cur != end; ++cur) {
-            pass_filters &= (*cur)(f);
+            pass_filters &= (*cur)(current);
         }
 
         if (pass_filters) {
-            std::cout << f.name() << std::endl;
+            std::cout << current.name() << std::endl;
         }
     }
 
@@ -59,6 +63,7 @@ int main(int argc, char *argv[]) {
     desc.add_options()
         ("help,h", "Print a summary of the command-line usage of iskat and exit.")
         ("version,v", "Print the iskat version number and exit.")
+        ("start,S", value<string>()->default_value("."), "Starting path")
         ("max-depth,D", value<int>(), "Descend  at  most levels (a non-neg"
         "ative integer) levels of directories below the commandline arguments.")
         ("follow-syml,F", "Follow symbolic links.")
@@ -70,24 +75,33 @@ int main(int argc, char *argv[]) {
         ("permissions,p", value<string>(), "File's permission bits are exactly mode")
         ("uid,u", value<int>(), "File's numeric user ID is n")
         ("gid,g", value<int>(), "File's numeric group ID is n")
-        ("owner,o", value<string>(), "File is owned by o")
+        ("owner,u", value<string>(), "File is owned by o")
         ("group,G", value<string>(), "File belongs to group g")
-        ("newer,n", value<string>(), "File modified after time point")
+        ("newer,N", value<string>(), "File modified after time point")
         ("older,O", value<string>(), "File modified before time point")
-        ("name", value<string>(), "File name")
+        ("name,n", value<string>(), "File name")
+        ("size,s", value<std::vector<string>>()->composing(), "File size")
 
     ;
 
     po::options_description action_options("actions");
     action_options.add_options()
         ("print,P", "Print the full file name")
+        ("exec,E", "execute command")
     ;
 
     desc.add(filter_options).add(action_options);
 
+    po::positional_options_description positional_args;
+    positional_args.add("start", 1);
+
 
     try {
-        po::store(po::command_line_parser(argc, argv).options(desc).run(), pasred_options);
+        po::store(po::command_line_parser(argc, argv)
+            .options(desc)
+            .positional(positional_args)
+            .run(), pasred_options);
+
         po::notify(pasred_options);
     }
     catch (std::exception& e) {
@@ -104,6 +118,11 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    walk(".", pasred_options);
+    try {
+        walk(pasred_options["start"].as<string>(), pasred_options);
+    } 
+    catch (std::exception & e) {
+        std::cout << e.what() << std::endl;
+    }
     return 0;
 }
